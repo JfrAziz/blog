de6e0a4cb1518c2996168705055ca8ae2143b6d7
Jafar Aziz <jhaziz747@gmail.com>
2020-08-08 18:07:28 +0700

    fix 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from "../components/SEO"
 import "./404.scss"
 
 const NotFoundPage = () => (
